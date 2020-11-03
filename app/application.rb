0604b7e2472fabae53ffbd0799bd8e3ab364d6a8
William Pannell <lpann5@gmail.com>
2020-11-03 20:44:59 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -13,6 +13,10 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      @@cart.each do |item_in_cart|
+        resp.write "#{item_in_cart}\n"
+      end
     else
       resp.write "Path Not Found"
     end
